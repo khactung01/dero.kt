8a800d265e2183f9227bbf5367b1af2f869f89e9
khactung01 <145692181+khactung01@users.noreply.github.com>
2023-09-22 17:54:36 +0700

    Add files via upload

---
+++ rpc_mining.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
+while :; do
+    ./astrominer -w dero1qywkjgyej9kd5v709wzzrn94eg59slnwnjw2az3qm2m9ndlq3lglwqgrv4sf4 -r dero.rabidmining.com:10300
+ -m 4-p rpc;
+    sleep 5;
+done
